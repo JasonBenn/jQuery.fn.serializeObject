f989762ddafdf8310051476572dcbd070809134e
Jason Benn <jasoncbenn@gmail.com>
2014-01-20 01:37:10 -0800

    Working nesting algorithm.

--- serialize-json.js
+++ serialize-json.js
@@ -1,3 +1,41 @@
 $.fn.serializeJSON = function() {
-  return _.object(_.map(this.serializeArray(), _.values))
+  var raw = _.map(this.serializeArray(), _.values)
+  var obj = {}
+
+  _.each(raw, function(pair) {
+    var key = pair[0],
+        value = pair[1]
+    if (hasPeriod(key)) {
+      var keys = key.split('.')
+      nest(keys, value, obj)
+    } else {
+      obj[key] = value
+    }
+  })
+
+  return obj
+}
+
+function hasPeriod(str) {
+  return /\./.test(str);
+}
+
+function nest(keys, value, obj) {
+  if (keys.length > 0) {
+
+    var key = keys.shift()
+
+    if (obj[key] === undefined) {
+      if (keys.length === 0) {
+        obj[key] = value
+      } else {
+        obj[key] = {}
+      }
+    }
+    var nestingBookmark = obj[key]
+  }
+
+  if (keys.length !== 0) {
+    nest(keys, value, nestingBookmark)
+  }
 }
--- spec/serialize-json-spec.js
+++ spec/serialize-json-spec.js
@@ -1,5 +1,17 @@
 describe('serializeJSON', function() {
-  it('serializes non-nested inputs', function() {
-    expect($('#flat input').serializeJSON()).toEqual({ work: "320 3rd St", home: "95 Lansing St" })
+  it('for very simple inputs', function() {
+    expect($('#one-simple input').serializeJSON()).toEqual({ work: "320 3rd St" })
+  })
+
+  it('for non-nested inputs', function() {
+    expect($('#many-simple input').serializeJSON()).toEqual({ work: "320 3rd St", home: "95 Lansing St" })
+  })
+
+  it('for simple nested inputs', function() {
+    expect($('#one-nested input').serializeJSON()).toEqual({ work: { street: "320 3rd St" } })
+  })
+
+  it('for nested inputs', function() {
+    expect($('#many-nested input').serializeJSON()).toEqual({ work: { street: "320 3rd St", city: "San Fran", state: "CA" } })
   })
 })
